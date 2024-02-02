120bed0e4c05233f0a655b73f0e3174aba3163a6
Haiyah Perez <haiyahperez@pursuit.org>
2024-02-02 13:02:25 -0500

    landing started

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,6 +1,7 @@
 import { Link, Navigate, useNavigate } from 'react-router-dom';
 import React, { useState } from 'react';
 import About from "./About.jsx";
+import "./Landing.css"
 
 const Landing = () => {
   const [index, setIndex] = useState(0);
@@ -26,16 +27,16 @@ const Landing = () => {
       <button onClick={handlePrevious}>Previous</button>
       <button onClick={handleNext}>Next</button>
       <div className="carousel-content">
-      <div className={index === 0 ? "carousel-item active" : "carousel-item"}>
-        <img src="https://res.cloudinary.com/dgifdj6nx/image/upload/v1706806167/recipEATS-LandingPage2_lxm0k8.jpg" alt="People Feasting"></img>
+        <div className={index === 0 ? "carousel-item active" : "carousel-item"}>
+          <img src="https://res.cloudinary.com/dgifdj6nx/image/upload/v1706806167/recipEATS-LandingPage2_lxm0k8.jpg" alt="People Feasting"></img>
+        </div>
+        <div className={index === 1 ? "carousel-item active" : "carousel-item"}>
+          <img src="https://res.cloudinary.com/dgifdj6nx/image/upload/v1706807150/recipEATS-LandingCarousel_isbl3d.jpg" alt="Pasta"></img>
+        </div>
+        <div className={index === 2 ? "carousel-item active" : "carousel-item"}>
+        <img src="https://res.cloudinary.com/dgifdj6nx/image/upload/v1706807152/recipEATS-LandingCarousel5_qrb79v.jpg" alt="Chocolate Bundt Cake"></img>
+        </div>
       </div>
-      <div className={index === 1 ? "carousel-item active" : "carousel-item"}>
-        <img src="https://res.cloudinary.com/dgifdj6nx/image/upload/v1706807150/recipEATS-LandingCarousel_isbl3d.jpg" alt="Pasta"></img>
-      </div>
-      <div className={index === 2 ? "carousel-item active" : "carousel-item"}>
-       <img src="https://res.cloudinary.com/dgifdj6nx/image/upload/v1706807152/recipEATS-LandingCarousel5_qrb79v.jpg" alt="Chocolate Bundt Cake"></img>
-      </div>
-    </div>
     </div>
     <div className="heading">
      <h1>RecipEATS!</h1>
