7c2466c7f311ab38e55b8227c1b08404dea04fed
Timothy Natal <timothynatal@pursuit.org>
2024-02-06 11:29:22 -0500

    Styling for Recipe-Details

--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -20,22 +20,42 @@ export default function RecipeDetails(){
 
     return (
         <div className="recipe-detail-wrapper">
-            <div className="recipe-info">
-                <h2>{recipe.dishName}</h2>
-                <img src={recipe.dishImage}/>
-                <p>Country of Origin: {recipe.countryOfOrigin}</p>
-                <p>Vegetarian: {recipe.isVegetarian ? "✅":"❌"} </p>
-                <p>Vegan: {recipe.isVegan ? "✅":"❌"}</p>
-                <p>Gluten Free: {recipe.isGlutenFree ? "✅":"❌"}</p>
-                <p>Spice Level: {recipe.extraInfo.spiceLevel}</p>
+            <div className="recipe-detail-body">
+                <div className="recipe-info">
+                    <div className="recipe-head">
+                        <div className="recipe-header">
+                            <h2>{recipe.dishName}</h2>
+                            <p>Country of Origin: {recipe.countryOfOrigin}</p>
+                            <p>Vegetarian: {recipe.isVegetarian ? "✅":"❌"} </p>
+                            <p>Vegan: {recipe.isVegan ? "✅":"❌"}</p>
+                            <p>Gluten Free: {recipe.isGlutenFree ? "✅":"❌"}</p>
+                            <p>Spice Level: {recipe.extraInfo.spiceLevel}</p>
+                            <div className="recipe-ingredients">
+                                <ul> 
+                                    <h4>Ingredients Needed:</h4>      
+                                    {recipe.ingredients.map(ingredient=>{
+                                        return (
+                                            <li>{ingredient}</li>
+                                        )   
+                                    })}
+                                </ul>
+                            </div>
+                        </div>
+                        <img src={recipe.dishImage}/>
+                    </div>
+                </div>
+                    <div className="recipe-prep-wrapper">
+                        <div className="review-button-wrapper">
+                            <Link  className="button-to-review" to={`/recipes/${id}/review`}>
+                                <button> Add Review</button>
+                            </Link>
+                        </div>
+                        <div className="recipe-prep">
+                            <p> Meal Prep: {recipe.extraInfo.prepTime}</p>
+                            <p>Instructions: {recipe.extraInfo.preparation}</p>
+                        </div>
+                    </div> 
             </div>
-            <div className="recipe-instructions">
-                <p> Meal Prep: {recipe.extraInfo.prepTime}</p>
-                <p>Instructions: {recipe.extraInfo.preparation}</p>
-            </div>
-           <Link to={`/recipes/${id}/review`}>
-            <button> Add a review</button>
-           </Link>
         </div>
     )
 }
